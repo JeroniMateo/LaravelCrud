f6246559cae560bea3dfb8ced8011bd684bf8a17
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-25 20:00:02 +0100

    Contactos Migration & Model

--- CrudLaravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ CrudLaravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
+            $table->string('apellidos');
             $table->string('email')->unique();
+            $table->integer('age');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ CrudLaravel/database/migrations/2022_01_25_185141_create_contactos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->integer('telefono');
+            $table->string('tipo');
+            $table->string('description');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
