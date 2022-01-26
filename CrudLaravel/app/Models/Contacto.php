0d6d48932d72c5bb5a8a64a27be1cda9e986eab7
Jeroni <jmateo@cifpfbmoll.eu>
2022-01-26 11:07:09 +0100

    11. Contactos Model filable

--- CrudLaravel/app/Models/Contacto.php
+++ CrudLaravel/app/Models/Contacto.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contacto extends Model
 {
-    use HasFactory;
+    protected $fillable =['nombre','apellido','telefono'];
+
+    public function getRouteKeyName()
+    {
+        return 'nombre';
+    }
 }
