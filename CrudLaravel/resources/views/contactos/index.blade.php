c1a8823e4509003516ab23de52f4929fa26f94d8
Jeroni <jmateo@cifpfbmoll.eu>
2022-02-03 19:29:27 +0100

    bootsrap div col class

--- CrudLaravel/resources/views/contactos/index.blade.php
+++ CrudLaravel/resources/views/contactos/index.blade.php
@@ -4,7 +4,7 @@
 
 
 @section('content')
-<h1>@lang('contactos')</h1>
+<h1>@lang('Phonebook')</h1>
 <style>
     b{
         font-size: 3vh
@@ -16,23 +16,41 @@
 
 
 <!--COn un foreach nos imprime todos los contactos de la agenda-->
-<ul> @forelse($contactos as $contacto)
-    <button><a href="{{ route('contactos.edit', $contacto) }}">@lang('Edit Contact')</a></button>
-    <h1>{{ $contacto->nombre }} {{ $contacto->apellidos }}</h1>  
-        <label for=""> <b>@lang('Telephon'):</b> {{ $contacto->telefono }}</label>
-        <p>  <b>@lang('Type'):</b>{{ $contacto->tipo }}</p> 
-        <p>  <b>@lang('telephone'):</b> {{ $contacto->birthday }}</p>
-        <p> <b>@lang('Relationship'):</b> {{ $contacto->relationship }}</p>
-        <p>{{ $contacto->description }}</p>   
-        <p>{{ $contacto->favorites }}</p></li> 
-            {{$contacto->updated_at->diffForHumans() }}
-            <br>
-
-<!--En caso de que esta vacia nos sale el siguente mensaje-->
+@forelse($contactos as $contacto)
+<div class="row g-3 p-2">
+    <div class="row mb-3 gx-3">
+        <div class="col col-md-2">
+            <h1>{{ $contacto->nombre }} {{ $contacto->apellidos }}</h1>  
+        </div>
+        <div class="col col-md-2">
+            <p><b>@lang('Telephon'):</b> {{ $contacto->telefono }}</p>
+        </div>
+        <div class="col col-md-2">
+            <p><b>@lang('Type'):</b>{{ $contacto->tipo }}</p> 
+        </div>
+        <div class="col col-md-1">
+            <p><b>@lang('telephone'):</b> {{ $contacto->birthday }}</p>
+        </div>
+        <div class="col col-md-1">
+            <p><b>@lang('Relationship'):</b> {{ $contacto->relationship }}</p>
+        </div>
+        <div class="col col-md-2">
+            <p>{{ $contacto->description }}</p>   
+        </div>
+        <div class="col col-md-1">
+            <small>{{ $contacto->favorites }}</small>
+        </div>
+        <div class="col col-md-1"> 
+            <button><a href="{{ route('contactos.edit', $contacto) }}">@lang('Edit Contact')</a></button>
+        </div>
+    </div>
+</div>
+    
+    <!--En caso de que esta vacia nos sale el siguente mensaje-->
     @empty
-    <li>@lang('Phonebook Empty')</li>
+        <li>@lang('Phonebook Empty')</li>
 
-    @endforelse
+        @endforelse
 </ul>
 
 @endsection
--- CrudLaravel/resources/views/form.blade.php
+++ CrudLaravel/resources/views/form.blade.php
@@ -1,99 +1,127 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <style>
-      form{
-        margin-left: 2vh
-      }
-     
-    </style>
-<div class="modal-body">
-<!--Este es el formulario que sale cuando queremos crear o editar un contacto
-donde tiene old values para que se guarden los cambios automaticamente y los mensajes de error de validacion-->    
-<form class="row g-3 p-2" action="" id="phonebook">
-        @csrf
-<!--En este fichero blade es donde sale los errores de validacion
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <style>
+    form {
+      margin-left: 2vh
+    }
+  </style>
+  <div class="modal-body">
+    <!--Este es el formulario que sale cuando queremos crear o editar un contacto
+donde tiene old values para que se guarden los cambios automaticamente y los mensajes de error de validacion-->
+    <form class="row g-3 p-2" action="" id="phonebook">
+      @csrf
+      <!--En este fichero blade es donde sale los errores de validacion
 Salen tanto al incio como debajo de cada uno de los respectivos inputs-->
-        @include('partials.validation-errors')  
-<div class="row mb-3 gx-3">      
-  <!--Nombre del Contacto-->
-  <div class="col col-md-4">
-    <label for="" class="form-label"><b>@lang('name'):</b></label>
-    <input type="text" name="nombre" id="" value="{{old('nombre',$contactos->nombre)}}"  class="form-control" required>
-      {!! $errors->first('nombre','<small>:message</small><br>') !!}
-  </div>
-<!--Apellido del Contacto-->
-  <div class="col col-md-4">
-    <label for="" class="form-label"><b>@lang('lastname'):</b></label>
-          <input type="text" name="apellidos" id="" value="{{old('apellidos', $contactos->apellidos)}}" class="form-control" required>
-        {!! $errors->first('apellidos','<small>:message</small><br>') !!} 
-  </div>
-  
-  <div class="col col-4">
-    <!--Telefono del Contacto-->
-    <label for="" class="form-label"><b>@lang('telephone'):</b></label>
-    <input type="number" name="telefono" id="" value="{{old('telefono', $contactos->telefono)}}" class="form-control" required>
-    {!! $errors->first('telefono','<small>:message</small><br>') !!}
-  </div>
-</div>
-  
+      @include('partials.validation-errors')
+      
+      <!--Nombre del Contacto-->
+      <div class="row mb-3 gx-3">
+        <div class="col col-md-1">
+          <label for="" class="form-label"><b>@lang('name'):</b></label>
+          <input type="text" name="nombre" id="" value="{{old('nombre',$contactos->nombre)}}" class="form-control"
+            required>
+          {!! $errors->first('nombre','<small>:message</small><br>') !!}
+        </div>
 
-  <div class="col-6">
-<!--Tipo del Contacto seleccionar si el telefono es Fijo o Movil-->
-    <label for="" class="form-label"><b>@lang('type'):</b></label>
-      <select name="tipo" id="tipo" form="phonebook" class="form-control" value="{{old('tipo', $contactos->tipo)}}">
-        <option value="Movil" class="form-control">Movil</option>
-        <option value="Fijo" class="form-control">Fijo</option>
-      </select>
-        {!! $errors->first('tipo','<small>:message</small><br>') !!}
-  </div>
-
-  <div class="col-12">
-<!--Cumpleaños del Contacto-->
-    <label for="" class="form-label"><b>@lang('birthday'):</b></label>
-    <input type="date" name="birthday" id="birthday" value="{{old('birthday', $contactos->birthday)}}">
-      {!! $errors->first('birthday','<small>:message</small><br>') !!}
-  </div>
-
-
-<!--Vinculo del Contacto-->
-        <label for="">
-          <br>@lang('relationship'):</br>
-          <input type="radio" name="relationship" id="family" value="{{old('relationship', $contactos->relationship)}}">
-          <label for="family">Family</label><br>
-          <input type="radio" name="relationship" id="friends" value="{{old('relationship', $contactos->relationship)}}">
-          <label for="friends">Friends</label><br>
-          <input type="radio" name="relationship" id="others" value="{{old('relationship', $contactos->relationship)}}">
-          <label for="Others">Others</label><br> 
-        </label> <br>
-        {!! $errors->first('relationship','<small>:message</small><br>') !!}
         <br>
+        <!--Apellido del Contacto-->
+        <div class="col col-md-1">
+          <label for="" class="form-label"><b>@lang('lastname'):</b></label>
+          <input type="text" name="apellidos" id="" value="{{old('apellidos', $contactos->apellidos)}}"
+            class="form-control" required>
+          {!! $errors->first('apellidos','<small>:message</small><br>') !!}
+        </div>
+        <br>
+
+        <!--Telefono del Contacto-->
+        <div class="col col-1">
+          <label for="" class="form-label"><b>@lang('telephone'):</b></label>
+          <input type="number" name="telefono" id="" value="{{old('telefono', $contactos->telefono)}}"
+            class="form-control" required>
+          {!! $errors->first('telefono','<small>:message</small><br>') !!}
+        </div>
+        <br>
+
+        <!--Tipo del Contacto seleccionar si el telefono es Fijo o Movil-->
+        <div class="col-1">
+          <label for="" class="form-label"><b>@lang('type'):</b></label>
+          <select name="tipo" id="tipo" form="phonebook" class="form-control" value="{{old('tipo', $contactos->tipo)}}">
+            <option value="Movil" class="form-control">Movil</option>
+            <option value="Fijo" class="form-control">Fijo</option>
+          </select>
+          {!! $errors->first('tipo','<small>:message</small><br>') !!}
+        </div>
+        <br>
+
+        <!--Cumpleaños del Contacto-->
+        <div class="col-1">
+          <label for="" class="form-label"><b>@lang('birthday'):</b></label>
+          <input type="date" name="birthday" id="birthday" value="{{old('birthday', $contactos->birthday)}}">
+          {!! $errors->first('birthday','<small>:message</small><br>') !!}
+        </div>
+        <br>
+
+        <!--Vinculo del Contacto-->
+        <div class="col-1">
+          <label for="">
+            <b>@lang('relationship'):</b>
+            <input type="radio" name="relationship" id="family"
+              value="{{old('relationship', $contactos->relationship)}}">
+            <label for="family">Family</label><br>
+            <input type="radio" name="relationship" id="friends"
+              value="{{old('relationship', $contactos->relationship)}}">
+            <label for="friends">Friends</label><br>
+            <input type="radio" name="relationship" id="others"
+              value="{{old('relationship', $contactos->relationship)}}">
+            <label for="Others">Others</label><br>
+          </label>
+          {!! $errors->first('relationship','<small>:message</small><br>') !!}
+        </div>
+        <br>
+        
         <!--Descripcion del Contacto-->
-        <label for="" class="form-label">
-          <b>@lang('description'):</b>
-          <textarea name="description" id="" cols="30" rows="10" value="{{old('description', $contactos->description)}}"  class="form-control"></textarea>
-        </label><br>
-        {!! $errors->first('description','<small>:message</small><br>') !!}
+        <div class="col-2">
+          <label for="" class="form-label">
+            <b>@lang('description'):</b>
+            <textarea name="description" id="" cols="20" rows="2"
+              value="{{old('description', $contactos->description)}}" class="form-control"></textarea>
+          </label>
+          {!! $errors->first('description','<small>:message</small><br>') !!}
+        </div>
         <br>
+        
         <!--Añadir el Contacto a favoritos mediante un checkbox-->
-        <input type="checkbox" name="favorites" id="favorites" value="fav" value="{{old('favorites', $contactos->favorites)}}">
-        <label for="favorites"><b>@lang('favorites')</b></label>
+        <div class="col-1">
+          <input type="checkbox" name="favorites" id="favorites" value="fav"
+            value="{{old('favorites', $contactos->favorites)}}">
+          <label for="favorites"><b>@lang('favorites')</b></label>
+        </div>
         <br>
-        <br>
-        <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+
+        <div class="col-1">
+          <button type="submit" class="btn btn-primary mb-3">Enviar</button>
     </form>
   </div>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+  </div>
+  </div>
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+</body>
+
 </html>
